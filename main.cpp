015a9d23c2a4fdc988a7cd6b84e905410f328bf9
Bennet Otoho <bennet2000@outlook.com>
2026-07-23 14:45:11 +0100

    set up CMake

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	printf("Hello World");
+	printf("Hello World\n");
+	printf("We can build with CMake now!\n");
 	return 0;
 }
