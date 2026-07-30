ef26a3dfa5171113343ce2d4c33a00091ff69d92
Ifiokobong Etokakpan <ifiokobongetokakpan@gmail.com>
2026-07-30 13:17:10 +0100

    added options

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/app.hpp>
 #include <ftxui/dom/elements.hpp>
+#include <ftxui/component/component_options.hpp>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
@@ -8,20 +9,24 @@ int main(int argc, char** argv) {
 	using namespace ftxui;
  
   std::vector<std::string> entries = {
-      "Entry 1",
-      "Entry 2",
-      "Entry 3",
+      "Start",
+      "Continue",
+      "HighScore",
+      "Instructions",
+      "Exit",
+         
   };
   int selected = 0;
  
-  // Create a menu component
-  auto menu = Menu(&entries, &selected);
+  auto app = App::TerminalOutput();
+  MenuOption option;
+  option.on_enter = app.ExitLoopClosure();
+  auto menu = Menu(&entries, &selected, option);
  
   // You can decorate components using the pipe operator.
   auto component = menu | border;
  
   // Start the main loop
-  auto app = App::TerminalOutput();
   app.Loop(component);
 	
 	return 0;
