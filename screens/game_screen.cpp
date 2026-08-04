18ab961bfa96359ce0d162acdfd9b4a19ec1efe4
Bennet Otoho <bennet2000@outlook.com>
2026-08-04 10:00:08 +0100

    implement snake thickness

--- screens/game_screen.cpp
+++ screens/game_screen.cpp
@@ -14,11 +14,12 @@ enum direction_t { UP, RIGHT, DOWN, LEFT };
 
 const unsigned FIELD_WIDTH = 120;
 const unsigned FIELD_HEIGHT = 120;
+const unsigned SNAKE_THICKNESS = FIELD_WIDTH / 15;
 
 std::atomic_bool is_game_active = false;
 int snake_head_x = FIELD_WIDTH / 2;
 int snake_head_y = FIELD_HEIGHT / 2;
-unsigned snake_length = 5;
+unsigned snake_length = 16;
 direction_t snake_dir = RIGHT;
 
 Canvas game_canvas;
@@ -28,14 +29,23 @@ auto move_snake = [](App* app) {
     int snake_tail_x;
     int snake_tail_y;
 
-    switch(snake_dir) {
+    switch (snake_dir) {
       case UP: snake_tail_x = snake_head_x; snake_tail_y = snake_head_y + snake_length; break;
       case RIGHT: snake_tail_x = snake_head_x + snake_length; snake_tail_y = snake_head_y; break;
       case DOWN: snake_tail_x = snake_head_x; snake_tail_y = snake_head_y - snake_length; break;
       case LEFT: snake_tail_x = snake_head_x + snake_length; snake_tail_y = snake_head_y;
     }
 
-    game_canvas.DrawPointLine(snake_head_x, snake_head_y, snake_tail_x, snake_tail_y);
+    for (int i = -4; i + 4 < SNAKE_THICKNESS; ++i)
+      switch (snake_dir) {
+        case UP:
+        case DOWN:
+          game_canvas.DrawBlockLine(snake_head_x + i, snake_head_y, snake_tail_x + i, snake_tail_y);
+          break;
+        case RIGHT:
+        case LEFT:
+          game_canvas.DrawBlockLine(snake_head_x, snake_head_y + i, snake_tail_x, snake_tail_y + i);
+      }
 
     ++snake_head_x;
 
@@ -65,7 +75,7 @@ void start_game(App* app) {
 
   snake_head_x = FIELD_WIDTH / 2;
   snake_head_y = FIELD_HEIGHT / 2;
-  snake_length = 5;
+  snake_length = 16;
   snake_dir = RIGHT;
 
   game_task = std::async(std::launch::async, move_snake, app);
